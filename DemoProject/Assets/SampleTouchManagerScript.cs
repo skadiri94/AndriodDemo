c9eb9c2a9957b1aea872393a0aa8ef0a546fd1ad
elsen <skadiri94@yahoo.com>
2022-02-03 01:35:38 +0000

    Signed-off-by:<skadiri94@yahoo.com>

--- DemoProject/Assets/SampleTouchManagerScript.cs
+++ DemoProject/Assets/SampleTouchManagerScript.cs
@@ -5,11 +5,43 @@ using UnityEngine;
 public class SampleTouchManagerScript : MonoBehaviour, ITouchController
 {
 
+
     IInteractable selected_object;
+    float starting_distance_to_selected_object;
     public void drag(Vector2 current_position)
     {
-        throw new System.NotImplementedException();
-    }
+      
+        Ray ourRay = Camera.main.ScreenPointToRay(current_position);
+
+        Debug.DrawRay(ourRay.origin, 30 * ourRay.direction);
+
+            RaycastHit info;
+            if (Physics.Raycast(ourRay, out info))
+            {
+                IInteractable objectHit = info.transform.GetComponent<IInteractable>();
+        
+                selected_object = objectHit;
+            Vector3 a = Camera.main.transform.position;
+            Vector3 b = info.transform.position;
+       
+            b.z = 0;
+
+          
+     
+            starting_distance_to_selected_object = Vector3.Distance(a, b);
+
+                    Ray new_positional_ray = Camera.main.ScreenPointToRay(current_position);
+
+                    { (selected_object as CubeControl).MoveTo(new_positional_ray.GetPoint(starting_distance_to_selected_object)); }
+
+                    // selected_object.MoveTo(new_positional_ray.GetPoint(starting_distance_to_selected_object));
+            
+        }
+
+        
+
+    
+}
 
     public void pinch(Vector2 position_1, Vector2 position_2, float relative_distance)
     {
@@ -20,6 +52,7 @@ public class SampleTouchManagerScript : MonoBehaviour, ITouchController
     {
         print("Im the manager and I recieved a tap from gesture");
 
+
         Ray our_ray = Camera.main.ScreenPointToRay(position);
         Debug.DrawRay(our_ray.origin, our_ray.direction * 50, Color.red, 4f);
         RaycastHit hit_info;
@@ -27,9 +60,10 @@ public class SampleTouchManagerScript : MonoBehaviour, ITouchController
         {
             IInteractable the_object = hit_info.transform.GetComponent<IInteractable>();
 
+           
             the_object.select_toggle();
             selected_object = the_object;
-
+            
 
             if (the_object is CubeControl)
             { (the_object as CubeControl).Do_cube_stuff(); }
