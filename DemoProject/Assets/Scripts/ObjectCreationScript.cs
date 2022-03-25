3088abdf29bca90cf8b7a0359a71b1ac8c48394b
Suleman Kadiri <skadiri94@outlook.com>
2022-03-25 16:33:36 +0000

    Final Update

--- DemoProject/Assets/Scripts/ObjectCreationScript.cs
+++ DemoProject/Assets/Scripts/ObjectCreationScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ObjectCreationScript : MonoBehaviour
 {
@@ -14,13 +15,49 @@ public class ObjectCreationScript : MonoBehaviour
     Material grass;
     GameObject gestureIndentifier;
     GameObject touchManager;
+    GameObject buttonGameObject;
+    Canvas canvas;
+    GameObject resetButton;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //Adding Eventsystem.
+        if (FindObjectOfType<EventSystem>() == null)
+        {
+            var eventSystem = new GameObject("EventSystem", typeof(EventSystem), typeof(StandaloneInputModule));
+        }
+
+        //Canvas & button creation
+        buttonGameObject = new GameObject("Button Canvas");
+        buttonGameObject.AddComponent<Canvas>();
 
 
+        canvas = buttonGameObject.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.gameObject.layer = LayerMask.NameToLayer("UI");
+        canvas.transform.position = new Vector3(960, 540, 0);
+        buttonGameObject.AddComponent<CanvasScaler>();
+        buttonGameObject.AddComponent<GraphicRaycaster>();
+
+        resetButton = DefaultControls.CreateButton(new DefaultControls.Resources());
+        resetButton.transform.SetParent(canvas.transform, false);
+        RectTransform rt = resetButton.GetComponent<Button>().GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(250, 120);
+        rt.anchoredPosition = new Vector3(-810, 465, -18);
+        resetButton.GetComponent<Button>().GetComponent<Image>().sprite = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UIMask.psd"); ;
+        resetButton.GetComponent<Button>().onClick.AddListener(resetGame);
+   
+        resetButton.GetComponentInChildren<Text>().text = "Reset";
+        resetButton.GetComponentInChildren<Text>().fontSize = 50;
+
+
+
+
+        Camera.main.gameObject.AddComponent<CameraControl>();
+
+    
 
         gestureIndentifier = new GameObject("GestureIdentifier");
         gestureIndentifier.AddComponent<GestureIdentifierScript>();
@@ -60,8 +97,9 @@ public class ObjectCreationScript : MonoBehaviour
         
     }
 
-    //void OnClick()
-    //{
-    //    SceneManager.LoadScene("SampleScene");
-    //}
+    void resetGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Android game using unity
 
-To run the Script in a new project, first, create a scene and make the below Layer:
+To run the Script in a new project, first, create a Basic(Built-in) scene and make the below Layer:
 
 3: Ground
 
-Now add the "ObjectCreationScript" and "CameraControl" to the Main Camera of the newly created scene play. All objects should be generated and work as expected.
+Now add the "ObjectCreationScript" to the Main Camera of the newly created scene play. All objects should be generated and work as expected.
 
 The following touch gestures were implemented:
 
@@ -16,3 +16,4 @@ Pinch - scales the object if selected: or zooms in the camera if no object is se
 Rotate - rotates a selected object or the camera if non is selected.
 Two fingers drag - Rotates the camera in a fixed position to either up/down or left/right.
 Press - If an object is selected and the finger held for a couple of seconds, the object starts shaking, or the camera starts shaking if no object is selected.
+Reset Button - Resets the Scene.
